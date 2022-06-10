f6b883a5a4a46fc7007f8b17e78fbe59dfe6111d
Gourav19042000 <59428586+Gourav19042000@users.noreply.github.com>
2022-06-11 00:25:50 +0530

    Time: 37 ms (75.26%), Space: 57.9 MB (70.97%) - LeetHub

--- 99-recover-binary-search-tree/99-recover-binary-search-tree.cpp
+++ 99-recover-binary-search-tree/99-recover-binary-search-tree.cpp
@@ -13,24 +13,44 @@ class Solution {
 public:
     
     //brute force solution!!!sort and match!o(2n+nlogn)!
-   vector<int> v;int i=0;
-    void inorder(TreeNode *root){
+//    vector<int> v;int i=0;
+//     void inorder(TreeNode *root){
+//         if(root==NULL)return;
+//         inorder(root->left);
+//         v.push_back(root->val);
+//         inorder(root->right);
+//     }
+//     void maketree(TreeNode *root){
+//         if(!root)return ;
+//         maketree(root->left);
+//         if(root->val!=v[i])swap(root->val,v[i]);
+//         i++;
+//         maketree(root->right);
+        
+//     }
+//     void recoverTree(TreeNode* root) {
+//       inorder(root);
+//         sort(v.begin(),v.end()); 
+//         maketree(root);
+//     }
+      void func(TreeNode *root,TreeNode* &prev,TreeNode* &first,TreeNode* &second){
         if(root==NULL)return;
-        inorder(root->left);
-        v.push_back(root->val);
-        inorder(root->right);
-    }
-    void maketree(TreeNode *root){
-        if(!root)return ;
-        maketree(root->left);
-        if(root->val!=v[i])swap(root->val,v[i]);
-        i++;
-        maketree(root->right);
+        func(root->left,prev,first,second);
+        if(prev!=NULL && root->val<=prev->val){
+            if(first==NULL)
+                first=prev;
+            second=root;
+        }
+        prev=root;
+        func(root->right,prev,first,second);
         
     }
-    void recoverTree(TreeNode* root) {
-      inorder(root);
-        sort(v.begin(),v.end()); 
-        maketree(root);
+      void recoverTree(TreeNode* root) {
+        TreeNode *prev=NULL,*first=NULL,*second=NULL;
+        
+        func(root,prev,first,second);
+        
+       swap(first->val,second->val);
+        return;
     }
 };
