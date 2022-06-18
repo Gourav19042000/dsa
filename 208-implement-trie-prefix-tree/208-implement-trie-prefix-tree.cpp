067ff50e9653b35432bdc66726abc79620e7df80
Gourav19042000 <59428586+Gourav19042000@users.noreply.github.com>
2022-06-19 00:11:51 +0530

    Time: 92 ms (54.54%), Space: 45 MB (49.88%) - LeetHub

---
+++ 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.cpp
@@ -0,0 +1,53 @@
+// //direct copied soln of only thi squestion!!!
+// //modified using striver code!!next time revice with tis code!!
+struct TrieNode{
+    TrieNode *next[26];
+    bool is_word=false;
+};
+
+class Trie
+{
+    TrieNode *root;
+public:
+    Trie()
+    {
+        root = new TrieNode();
+    }
+
+    // Inserts a word into the trie.
+    void insert(string s)
+    {
+        TrieNode *p = root;
+        for(int i = 0; i < s.size(); ++ i)
+        {
+            if(p -> next[s[i] - 'a'] == NULL)
+                p -> next[s[i] - 'a'] =new TrieNode();
+          
+            p = p -> next[s[i] - 'a'];
+        }
+        p -> is_word = true;
+    }
+
+    // Returns if the word is in the trie.
+    bool search(string key)
+    {
+        TrieNode *p = find(key);
+        return p != NULL && p -> is_word;
+    }
+
+    // Returns if there is any word in the trie
+    // that starts with the given prefix.
+    bool startsWith(string prefix)
+    {
+        return find(prefix) != NULL;
+    }
+
+private:
+    TrieNode* find(string key)
+    {
+        TrieNode *p = root;
+        for(int i = 0; i < key.size() && p != NULL; ++ i)
+            p = p -> next[key[i] - 'a'];
+        return p;
+    }
+};
